86298de4c726384c89fd1d1d53502a8d63020327
Murillo Bilches Navarro <murillo.navarro@pitang.com>
2021-08-03 10:40:44 -0300

    Inserindo card

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,30 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
+import Card from './components/layout/Card'
 
 export default _ =>
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio min={1} max={60}/>
-        <Fragmento />
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro Silva"
-            nota={9.3}
-        />
-        <Primeiro></Primeiro>
+
+        <Card titulo="#04 - Desafio Aleatório">
+            <Aleatorio min={1} max={60}/>
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parâmetro">
+            <ComParametro
+                titulo="Situação do Aluno"
+                aluno="Pedro Silva"
+                nota={9.3}
+            />
+        </Card>
+
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
+
     </div>
