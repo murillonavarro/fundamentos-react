f7ce7234fc40e4b3d8ce636038d513c7ed620b1c
Murillo Bilches Navarro <murillo.navarro@pitang.com>
2021-08-03 10:58:17 -0300

    Trabalhando a responsividade dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
@@ -7,27 +8,28 @@ import Aleatorio from './components/basicos/Aleatorio'
 import Card from './components/layout/Card'
 
 export default _ =>
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React</h1>
 
-        <Card titulo="#04 - Desafio Aleatório">
-            <Aleatorio min={1} max={60}/>
-        </Card>
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório">
+                <Aleatorio min={1} max={60} />
+            </Card>
 
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
 
-        <Card titulo="#02 - Com Parâmetro">
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Pedro Silva"
-                nota={9.3}
-            />
-        </Card>
+            <Card titulo="#02 - Com Parâmetro">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3}
+                />
+            </Card>
 
-        <Card titulo="#01 - Primeiro Componente">
-            <Primeiro></Primeiro>
-        </Card>
-
+            <Card titulo="#01 - Primeiro Componente">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
