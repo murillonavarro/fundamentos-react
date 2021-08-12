d57178bf20e1ccb277c07da8a9d94046b9c0d4d5
Murillo Bilches Navarro <murillo.navarro@pitang.com>
2021-08-12 17:05:56 -0300

    Utilizando componente filho

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import React from 'react'
 
+import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
@@ -12,15 +14,23 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
-            <Card titulo="#04 - Desafio Aleatório">
+            <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Silva">
+                <FamiliaMembro nome="Pedro" />
+                <FamiliaMembro  nome="Ana" />
+                <FamiliaMembro  nome="Gustavo" />
+                </Familia>
+            </Card>
+
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60} />
             </Card>
 
-            <Card titulo="#03 - Fragmento">
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
                 <Fragmento />
             </Card>
 
-            <Card titulo="#02 - Com Parâmetro">
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
                 <ComParametro
                     titulo="Situação do Aluno"
                     aluno="Pedro Silva"
@@ -28,7 +38,7 @@ export default _ =>
                 />
             </Card>
 
-            <Card titulo="#01 - Primeiro Componente">
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
                 <Primeiro></Primeiro>
             </Card>
         </div>
